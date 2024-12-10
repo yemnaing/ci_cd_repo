529c6377fb60da69c220ce161552ed763e0eab09
yemnaing <yemyintnaing@gmail.com>
2024-12-10 22:48:55 +0630

    Update user_data.sh
    us

--- user_data.sh
+++ user_data.sh
@@ -7,6 +7,6 @@ sudo apt update
 sudo apt install ruby-full -y
 sudo apt install wget
 cd /home/ubuntu
-wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
